aa82613c0d0999ba20c0363d190ede7ec54bf257
swojeet <swojeetkayastha@gmail.com>
2016-09-17 15:48:11 +0545

    CRUD actions setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,50 @@
 class PostsController < ApplicationController
 	def index
-		
+      @posts = Post.all
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = current_user.posts.create(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :new, status: :inprocessable_entity
+		end
+	end
+
+	def edit
+		@post= Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def is_owner?
+		redirect_to root_path if Post.find(params[:id]).user != current_user
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:user_id, :description)
+	end
 end
